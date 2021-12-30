cb6509f499e3b2ada1ea482a75c04cc5c5095ea4
Elgin <elginlee_2000@hotmail.com>
2021-12-30 22:31:40 +0800

    modified index.html, style.css, app.js

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 let boxPerRow = 16;
 let boxPerCol = 16;
 let isDrawing = false;
+let paintColor = "green";
+let isRainbow = false;
 
 const gridContainer = document.querySelector(".grid-container");
 const resetBtn = document.querySelector(".reset-btn");
 const gridSizeSlider = document.querySelector(".grid-size-slider");
 const gridMagnitude = document.querySelector(".grid-size-mag");
+const paintButtons = Array.from(document.querySelectorAll(".paint-btn")).filter(colorTag => colorTag.textContent !== "Rainbow");
+const rainbowPaintBtn = document.getElementById("rainbow-btn");
 
 function paintHandler(e) {
     if (isDrawing) {
-        e.target.style.backgroundColor = "green";
+        e.target.style.backgroundColor = `${paintColor}`;
+    }
+
+    if (isDrawing && isRainbow) {
+        const colors = ["red", "orange", "yellow", "green", "indigo", "violet", "blue"];
+        const randomInt = Math.floor(Math.random() * (colors.length - 1 + 1) + 1);
+        e.target.style.backgroundColor = `${colors[randomInt]}`;
     }
 }
 
@@ -49,8 +59,33 @@ function deleteGrid() {
     gridContainer.innerHTML = ``;
 }
 
+function paintHoverHandler(e) {
+    e.target.style.backgroundColor = e.target.textContent;
+    e.target.style.boxShadow = `2px 2px 10px ${e.target.textContent}`
+}
+
+function resetPaint(e) {
+    e.target.style.backgroundColor = "white";
+    e.target.style.boxShadow = "none";
+}
+
+function changeColorHandler(e) {
+    paintColor = e.target.textContent;
+    isRainbow = false;
+
+    // Make color change permanent
+}
+
 resetBtn.addEventListener("click", resetHandler);
 
 gridSizeSlider.addEventListener("input", gridSizeHandler);
+
+paintButtons.forEach(paintBtn => {
+    paintBtn.addEventListener("mouseover", paintHoverHandler);
+    paintBtn.addEventListener("mouseleave", resetPaint);
+    paintBtn.addEventListener("click", changeColorHandler);
+});
+
+rainbowPaintBtn.addEventListener("click", () => { isRainbow = true });
 
 generateGrid();
